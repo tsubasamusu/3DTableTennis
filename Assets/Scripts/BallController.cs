69add61e61187416de6de2c7bf5589ce70e6b3d1
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-25 18:11:11 +0900

    Table.csを作成した。

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 /// <summary>
@@ -15,19 +16,37 @@ public class BallController : MonoBehaviour
 
     private bool isPlayerTurn;//プレーヤーのターンかどうか
 
+    private bool isBounded;//台上で跳ねたかどうか
+
+    /// <summary>
+    /// ボールを打つ
+    /// </summary>
+    /// <param name="direction">打つ方向</param>
+    public void ShotBall(Vector3 direction)
+    {
+        //まだ跳ねていない状態に切り替える
+        isBounded = false;
+    }
+
     /// <summary>
     /// 適切なy座標を取得する
     /// </summary>
+    /// <param name="inCourt">コートに入るかどうか</param>
     /// <returns>適切なy座標</returns>
-    private float GetAppropriatePosY()
+    private float GetAppropriatePosY(bool inCourt)
     {
         //跳ねる位置を取得
         Vector3 boundPos = isPlayerTurn ? playerBoundTran.position : enemyBoundTran.position;
 
         //跳ねる位置との距離を取得
-        float length = Mathf.Abs(Vector3.Scale((transform.position - boundPos), new Vector3(1f, 0f, 1f)).magnitude);
+        float length = (Vector3.Scale(transform.position, new Vector3(1f, 0f, 1f))- Vector3.Scale(boundPos, new Vector3(1f, 0f, 1f))).magnitude;
 
         //適切なy座標を返す
-        return -(2f / 25f)*(length - 5f)*(length - 5f) + 2f;
+        return-(0.75f/25f)*(length - 5f)*(length - 5f) + 1.5f;
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x,GetAppropriatePosY(true),transform.position.z);
     }
 }
---
+++ Assets/Scripts/Table.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ‘ì‹…‘ä‚ÉŠÖ‚·‚éˆ—‚ğs‚¤
+/// </summary>
+public class Table : MonoBehaviour
+{
+    
+}
