fa54a00cfeeed80e5b23ffa3803a4fe6db6810c9
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-28 09:38:52 +0900

    全てのUIをUIManagerに登録した。

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,64 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;//リストを使用
 using UnityEngine;
+using System;//Serializable属性を使用
+using UnityEngine.UI;//UIを使用
 
+/// <summary>
+/// UIを制御する
+/// </summary>
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// ロゴの種類
+    /// </summary>
+    private enum LogoType
     {
-        
+        Title,GameOver,GameClear//列挙子
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// ロゴのデータを管理する 
+    /// </summary>
+    [Serializable]
+    private class LogoData
     {
-        
+        public LogoType LogoType;//ロゴの種類
+        public Sprite sprite;//スプライト
+    }
+
+    [SerializeField]
+    private List<LogoData> logoDatasList = new();//ロゴのデータのリスト
+
+    [SerializeField]
+    private Image imgLogo;//ロゴのイメージ
+
+    [SerializeField]
+    private Image imgBackground;//背景のイメージ
+
+    [SerializeField]
+    private CanvasGroup cvScore;//得点のキャンバスグループ
+
+    [SerializeField]
+    private Text txtScore;//得点のテキスト
+
+    [SerializeField]
+    private Button button;//ボタン
+
+    [SerializeField]
+    private Image imgButton;//ボタンのイメージ
+
+    [SerializeField]
+    private Text txtButton;//ボタンのテキスト
+
+    /// <summary>
+    /// ロゴのスプライトを取得する
+    /// </summary>
+    /// <param name="logoType">ロゴの種類</param>
+    /// <returns>ロゴのスプライト</returns>
+    private Sprite GetLogoSprite(LogoType logoType)
+    {
+        //ロゴのスプライトを返す
+        return logoDatasList.Find(x => x.LogoType == logoType).sprite;
     }
 }
