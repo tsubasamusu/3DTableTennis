b8beab0a2ee3fa3b1cdcc70e2e52cd4f9c8d37c0
tsubasamusu <rdr2daisuki@gmail.com>
2022-10-01 14:30:25 +0900

    EnemyController.csにコメントを追記した。

--- Assets/Scripts/Refactoring/EnemyController.cs
+++ Assets/Scripts/Refactoring/EnemyController.cs
@@ -1,53 +1,58 @@
 using UnityEngine;
 
-namespace yamap {
-
+namespace yamap 
+{
     /// <summary>
     /// エネミーの行動を制御する
     /// </summary>
-    public class EnemyController : ControllerBase {
-
-        //[SerializeField]
+    public class EnemyController : ControllerBase 
+    {
         private BoundPoint enemyBoundPoint;//エネミーの跳ねる位置
 
-        //[SerializeField]
-        //private BallController ballController;//BallController
-
-        //private Vector3 firstPos;//初期位置
-
-
-
-        void Reset() {
-            if (TryGetComponent(out EnemyController enemyController)) {
+        /// <summary>
+        /// リセットする
+        /// </summary>
+        private void Reset() 
+        {
+            //自分がエネミーなら
+            if (TryGetComponent(out EnemyController _)) 
+            {
+                //所有者の種類をエネミーに設定
                 ownerType = OwnerType.Enemy;
             }
         }
 
         /// <summary>
-        /// 親クラスで用意している設定の処理(base 部分)は残しつつ、このクラス独自の処理を追加する
+        /// ControllerBaseの初期設定を行う（親クラスで用意している設定の処理は残しつつ、このクラス独自の処理を追加する）
         /// </summary>
-
-        public override void SetUpControllerBase(BallController ballController) {
+        public override void SetUpControllerBase(BallController ballController) 
+        {
+            //TODO:これはどういう意味？
             base.SetUpControllerBase(ballController);
 
+            //リセットする
             Reset();
 
-            enemyBoundPoint = ballController.GetBoundPoint(ownerType);  // BoundPoint は BallController で管理している情報なので、BallController からもらえばアサイン不要。
+            //エネミー用の跳ねる位置を取得
+            enemyBoundPoint = ballController.GetBoundPoint(ownerType);
         }
 
         /// <summary>
         /// 移動方向を取得する
         /// </summary>
-        /// <returns></returns>
-        protected override Vector3 GetMoveDir() {
+        /// <returns>移動方向</returns>
+        protected override Vector3 GetMoveDir() 
+        {
             //現在のボールの所有者がエネミーなら
-            if (ballController.CurrentOwner == OwnerType.Enemy) {
+            if (ballController.CurrentOwner == OwnerType.Enemy) 
+            {
                 //初期位置への方向を返す（初期位置に向かって移動する）
                 return firstPos - transform.position;
             }
 
             //相手（プレイヤー）のボールがコートに入るなら
-            if (ballController.InCourt) {
+            if (ballController.InCourt) 
+            {
                 //目的地への方向を返す（ボールに向かって移動する）
                 return ballController.transform.position - transform.position;
             }
@@ -56,29 +61,21 @@ namespace yamap {
             return Vector3.zero;
         }
 
-        ///// <summary>
-        ///// EnemyControllerの初期設定を行う
-        ///// </summary>
-        //public void SetUpEnemyController(BallController ballController) {
-        //    //BallControllerを取得
-        //    this.ballController = ballController;
-
-        //    //初期位置を取得
-        //    firstPos = transform.position;
-        //}
-
         /// <summary>
         /// ラケットを制御する（Updateメソッドで呼び出され続ける）
         /// </summary>
-        protected override void ControlRacket() {
+        protected override void ControlRacket() 
+        {
             //ボールが攻撃圏内に入っていなかったら
-            if (Mathf.Abs((transform.position - ballController.transform.position).magnitude) > GameData.instance.EnemyShotRange) {
+            if (Mathf.Abs((transform.position - ballController.transform.position).magnitude) > GameData.instance.EnemyShotRange) 
+            {
                 //以降の処理を行わない
                 return;
             }
 
             //ラケットを振っている最中なら
-            if (!racketController.IsIdle) {
+            if (!racketController.IsIdle) 
+            {
                 //以降の処理を行わない
                 return;
             }
@@ -90,7 +87,8 @@ namespace yamap {
         /// <summary>
         /// キャラクターの向きを設定する（Updateメソッドで呼び出され続ける）
         /// </summary>
-        protected override void SetCharaDirection() {
+        protected override void SetCharaDirection() 
+        {
             //常にエネミーのコートの方向に向く
             transform.LookAt(enemyBoundPoint.transform.position);
         }
